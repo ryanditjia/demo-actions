21d32eb9200418f97e76538d88f526d2bc824d85
Ryandi Tjia <ryandi.tjia@me.com>
2025-01-03 21:11:47 +0700

    json

--- dist/index.js
+++ dist/index.js
@@ -31770,6 +31770,14 @@ function parseParams (str) {
 module.exports = parseParams
 
 
+/***/ }),
+
+/***/ 2395:
+/***/ ((module) => {
+
+"use strict";
+module.exports = /*#__PURE__*/JSON.parse('{"name":"initialvalue"}');
+
 /***/ })
 
 /******/ 	});
@@ -31811,20 +31819,18 @@ module.exports = parseParams
 /******/ 	
 /************************************************************************/
 var __webpack_exports__ = {};
-const core = __nccwpck_require__(9999);
-const github = __nccwpck_require__(2819);
+const fs = __nccwpck_require__(9896)
+const core = __nccwpck_require__(9999)
+const github = __nccwpck_require__(2819)
 
 try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
+  const updatedValue = core.getInput('updated-value')
+  // update prod.json with updatedValue
+  const prodJson = __nccwpck_require__(2395)
+  prodJson.name = updatedValue
+  fs.writeFileSync('./prod.json', JSON.stringify(prodJson, null, 2))
 } catch (error) {
-  core.setFailed(error.message);
+  core.setFailed(error.message)
 }
 
 module.exports = __webpack_exports__;
--- index.js
+++ index.js
@@ -1,15 +1,13 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
+const fs = require('fs')
+const core = require('@actions/core')
+const github = require('@actions/github')
 
 try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
+  const updatedValue = core.getInput('updated-value')
+  // update prod.json with updatedValue
+  const prodJson = require('./prod.json')
+  prodJson.name = updatedValue
+  fs.writeFileSync('./prod.json', JSON.stringify(prodJson, null, 2))
 } catch (error) {
-  core.setFailed(error.message);
+  core.setFailed(error.message)
 }
