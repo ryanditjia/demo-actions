8f7c196438598b06fee6003854a4c4fef42c292d
Ryandi Tjia <ryandi.tjia@me.com>
2025-01-03 22:03:22 +0700

    update

--- index.js
+++ index.js
@@ -8,23 +8,28 @@ async function main() {
     const updatedValue = core.getInput('updated-value')
     const octokit = github.getOctokit(core.getInput('myToken'))
 
-    const prodJson = require('./prod.json')
-    prodJson.name = updatedValue
+    const owner = 'ryanditjia'
+    const repo = 'demo-actions'
+    const path = 'prod.json'
+    const branch = 'registry'
 
     const { data: currentFile } = await octokit.rest.repos.getContent({
       owner,
       repo,
-      path: 'prod.json',
-      ref: 'registry',
+      path,
+      ref: branch,
     })
 
+    const json = JSON.parse(Buffer.from(currentFile.content, 'base64').toString())
+    json.name = updatedValue
+
     const response = await octokit.rest.repos.createOrUpdateFileContents({
-      owner: 'ryanditjia',
-      repo: 'demo-actions',
-      path: 'prod.json',
+      owner,
+      repo,
+      path,
       message: 'feat: update prod.json',
-      content: Buffer.from(JSON.stringify(prodJson, null, 2)).toString('base64'),
-      branch: 'registry',
+      content: Buffer.from(JSON.stringify(json, null, 2)).toString('base64'),
+      branch: branch,
       sha: currentFile.sha,
     })
 
